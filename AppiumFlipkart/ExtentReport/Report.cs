7a71375c1f3b3cf243862b942f47d4a678ba4a87
sidthakur258 <sidthakur258@gmail.com>
2020-08-27 17:22:40 +0530

    ADDED:Functionality to generate extent report

---
+++ AppiumFlipkart/ExtentReport/Report.cs
@@ -0,0 +1,27 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace AppiumFlipkart.ExtentReport
+{
+    public class Report
+    {
+        private static ExtentReports extent;
+        private static ExtentHtmlReporter htmlReporter;
+
+        /// <summary>
+        /// To get instance of extent report
+        /// </summary>
+        /// <returns> extent class object</returns>
+        public static ExtentReports GetInstance()
+        {
+            if (extent == null)
+            {
+                string reportPath = @"C:\Users\sidth\source\repos\AppiumFlipkart\AppiumFlipkart\ExtentReport\Report.html";
+                htmlReporter = new ExtentHtmlReporter(reportPath);
+                extent = new ExtentReports();
+                extent.AttachReporter(htmlReporter);
+            }
+            return extent;
+        }
+    }
+}
