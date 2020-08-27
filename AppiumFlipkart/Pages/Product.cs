03ea54d12fd3c6a5fa106604d9dab06215e27ed4
sidthakur258 <sidthakur258@gmail.com>
2020-08-27 17:12:02 +0530

    ADDED:Functionality to Search Product & PlaceOrder in Flipkart App

---
+++ AppiumFlipkart/Pages/Product.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Product.cs" company="BridgeLabz">
+// Copyright (c) 2020 All Rights Reserved
+// </copyright>
+//-----------------------------------------------------------------------
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using SeleniumExtras.PageObjects;
+using System.Threading;
+
+namespace AppiumFlipkart.Pages
+{
+    /// <summary>
+    ///  Stores all web elements and functionality for searching product and placing order in flipkart
+    /// </summary>
+    public class Product
+    {
+        public AndroidDriver<AndroidElement> driver;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Product"/> class.
+        /// </summary>
+        /// <param name="driver">control application</param>
+        public Product(AndroidDriver<AndroidElement> driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        /// <summary>
+        /// Android elements
+        /// </summary>
+        [FindsBy(How = How.Id, Using = "com.flipkart.android:id/search_widget_textbox")]
+        public IWebElement searchBox;
+
+        [FindsBy(How = How.Id, Using = "com.flipkart.android:id/search_autoCompleteTextView")]
+        public IWebElement searchProduct;
+
+        [FindsBy(How = How.XPath, Using = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/" +
+            "androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout/" +
+            "android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]")]
+        public IWebElement firstResult;
+
+        [FindsBy(How = How.XPath, Using = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout" +
+            "/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout" +
+            "/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.ScrollView" +
+            "/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]")]
+        public IWebElement selectProduct;
+
+        [FindsBy(How = How.XPath, Using = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout" +
+            "/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout" +
+            "/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup" +
+            "/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup")]
+        public IWebElement goToCart;
+
+        [FindsBy(How = How.XPath, Using = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout" +
+            "/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout" +
+            "/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup" +
+            "/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]")]
+        public IWebElement placeOrder;
+
+        /// <summary>
+        /// Search product and place order
+        /// </summary>
+        public void SearchProductAndPlaceOrder()
+        {
+            searchBox.Click();
+            Thread.Sleep(2000);
+            searchProduct.SendKeys("iphone xr");
+            firstResult.Click();
+            Thread.Sleep(2000);
+            selectProduct.Click();
+            Thread.Sleep(2000);
+            goToCart.Click();
+            Thread.Sleep(5000);
+            placeOrder.Click();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# Flipkart-Appium
+## Project Title
+
+Flipkart Android application Automation using appium
+
+
+## Description
+
+* The project is to automate Flipkart application which is e-commerce application for buying electronic, fashion, home essentials,
+  groceries and lifestyle product
+
+## Prerequisites
+
+* Visual Studio 2019   
+* Internet Connection
+* Appium Desktop 
+* Android device
+* Usb debbuging unabled
+* Adb driver installed
+* SDK commandline installed
+
+## Technology Used
+
+* C#
+
+## Frameworks
+
+* .Net Framework  
+* Nunit  
+* Selenium  
+* Data Driven  
+
+## Design Pattern
+
+* Page Object Model
+
+## Packages
+
+* DotNetSeleniumExtras.PageObjects- For Page object Model  
+* ExtentReports- To generate Test Reports   
+* NUnit- To define test cases, assertion  
+* NUnit3Adapter- Running test cases in Visual Studio  
+* Selenium.WebDriver- .Net Binding for selenium webdriver API  
+* Newtonsoft.Json-To access data from json file
+* Appium.WebDriver-Selenium webdriver extension for handling android application
+* Selenium.Support-Provide support classes for Selenium webDriver
+* System.Configuration.ConfigurationManager-Provides type that supports configuration file
+
+## Test scenario covered
+ 
+* Launch Flipkart application 
+* Select Language
+* Change from mobile number login to email login  
+* Provide correct login credentials
+* Search for product in search bar
+* Select the first search result 
+* Add product to cart
+* Place order   
+  
+## Running the tests
+
+* Connect android device to system using usb cable
+* Navigate to tool bar and Click on Test  
+* Click on run all Test
+
+## Author
+
+* Siddhesh Thakur
